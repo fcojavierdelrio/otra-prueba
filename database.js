93c00ed25ac5b11ef0057e714e809cb3d2678c9c
Christian Meneses Álvarez <35198717+ChristianMenesesAlvarez@users.noreply.github.com>
2024-12-30 09:57:49 +0100

    update

--- database.js
+++ database.js
@@ -4,9 +4,6 @@ async function initializeSupabase() {
     const supabaseUrl = "https://ilzowclnsvabbpwnwkrt.supabase.co";
     const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlsem93Y2xuc3ZhYmJwd253a3J0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUzMTI2NjQsImV4cCI6MjA1MDg4ODY2NH0.KrPg77Bavax4DgHCpDou4L9XAB41II9_ywNcjy8w6ao'
     supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
-}
-
-function showSupabase() {
     console.log(supabase);
 }
 
@@ -20,6 +17,3 @@ async function insertRow(data) {
 }
 
 window.insertRow = insertRow;
-
-await initializeSupabase();
-showSupabase();
