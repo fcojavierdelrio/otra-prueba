eee941b196bc990611da90aa15750a0eac82a5b4
Christian Meneses Álvarez <35198717+ChristianMenesesAlvarez@users.noreply.github.com>
2024-12-30 10:07:18 +0100

    update

--- database.js
+++ database.js
@@ -1,6 +1,7 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js';
 const supabaseUrl = "https://ilzowclnsvabbpwnwkrt.supabase.co";
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlsem93Y2xuc3ZhYmJwd253a3J0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUzMTI2NjQsImV4cCI6MjA1MDg4ODY2NH0.KrPg77Bavax4DgHCpDou4L9XAB41II9_ywNcjy8w6ao'
-supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+supabase = createClient(supabaseUrl, supabaseKey);
 console.log(supabase);
 
 function insertRow(data) {
