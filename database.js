719423f2ecb62e598e93fc7da28254228fa95e54
Christian Meneses Álvarez <35198717+ChristianMenesesAlvarez@users.noreply.github.com>
2024-12-30 15:49:34 +0100

    update

--- database.js
+++ database.js
@@ -1,18 +1,13 @@
-function insertRow(row) {
-    const url = "https://ilzowclnsvabbpwnwkrt.supabase.co/rest/v1/PALETAS";
-    const apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlsem93Y2xuc3ZhYmJwd253a3J0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUzMTI2NjQsImV4cCI6MjA1MDg4ODY2NH0.KrPg77Bavax4DgHCpDou4L9XAB41II9_ywNcjy8w6ao";
-    const payload = {
-      PALETAS: row.PALETAS,
-      MATRICULA: row.MATRICULA
-    };
-  
-    fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "apikey": apiKey
-      },
-      body: JSON.stringify(payload)
-    })
-      .then(response => console.log(response.json()));
-  }
+const { createClient } = supabase
+const _supabase = createClient('https://ilzowclnsvabbpwnwkrt.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlsem93Y2xuc3ZhYmJwd253a3J0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUzMTI2NjQsImV4cCI6MjA1MDg4ODY2NH0.KrPg77Bavax4DgHCpDou4L9XAB41II9_ywNcjy8w6ao')
+
+async function insertRow(row) {
+  const { data, error } = await supabase
+    .from('PALETAS')
+    .insert([{ PALETAS: row.paletas, MATRICULA: row.matricula }])
+    .select();
+  console.log(data);
+  console.log(error);
+}
+
+insertRow({ MATRICULA: "1234ABC", PALETAS: 33 })
