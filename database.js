6dc9759f654a3aabb020307f213269a18345055f
Christian Meneses Álvarez <35198717+ChristianMenesesAlvarez@users.noreply.github.com>
2024-12-30 09:40:30 +0100

    update

---
+++ database.js
@@ -0,0 +1,12 @@
+const supabaseUrl = 'https://ilzowclnsvabbpwnwkrt.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlsem93Y2xuc3ZhYmJwd253a3J0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUzMTI2NjQsImV4cCI6MjA1MDg4ODY2NH0.KrPg77Bavax4DgHCpDou4L9XAB41II9_ywNcjy8w6ao'
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+async function insertRow(data) {
+    return await supabase
+      .from('PALETAS')
+      .insert([
+        { PALETAS: data.PALETAS, MATRICULA: data.MATRICULA },
+  ])
+      .select()
+}
---
+++ script.js
@@ -0,0 +1,40 @@
+const form = document.getElementById('multiStepForm');
+const numeroPaletasInput = document.getElementById('numeroPaletas');
+const matriculaCamionInput = document.getElementById('matriculaCamion');
+const formTitle = document.getElementById('formTitle');
+let currentStep = 1;
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    if (currentStep === 1) {
+        const numeroPaletas = numeroPaletasInput.value.trim();
+        if (numeroPaletas === "" || parseInt(numeroPaletas) <= 0) {
+            alert("Por favor, ingrese un número válido de paletas.");
+            return;
+        }
+
+        currentStep = 2;
+        numeroPaletasInput.style.display = "none";
+        matriculaCamionInput.style.display = "block";
+        matriculaCamionInput.setAttribute('required', true);
+        formTitle.textContent = "Ingrese la Matrícula del Camión";
+        matriculaCamionInput.focus();
+    } else if (currentStep === 2) {
+        const matriculaCamion = matriculaCamionInput.value.trim();
+        if (matriculaCamion === "") {
+            alert("Por favor, ingrese una matrícula válida.");
+            return;
+        }
+
+        insertRow({PALETAS:33, MATRICULA: '1234ABC'})
+        alert(`Número de paletas: ${numeroPaletasInput.value}\nMatrícula del camión: ${matriculaCamion}`);
+
+        form.reset();
+        currentStep = 1;
+        matriculaCamionInput.style.display = "none";
+        matriculaCamionInput.removeAttribute('required');
+        numeroPaletasInput.style.display = "block";
+        formTitle.textContent = "Ingrese el Número de Paletas";
+    }
+});
