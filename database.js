6df9bf7fcab42213c81e11810ffc68897031eaea
Christian Meneses Álvarez <35198717+ChristianMenesesAlvarez@users.noreply.github.com>
2024-12-30 10:04:06 +0100

    update

--- database.js
+++ database.js
@@ -1,13 +1,9 @@
-let supabase;
+const supabaseUrl = "https://ilzowclnsvabbpwnwkrt.supabase.co";
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlsem93Y2xuc3ZhYmJwd253a3J0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUzMTI2NjQsImV4cCI6MjA1MDg4ODY2NH0.KrPg77Bavax4DgHCpDou4L9XAB41II9_ywNcjy8w6ao'
+supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+console.log(supabase);
 
-async function initializeSupabase() {
-    const supabaseUrl = "https://ilzowclnsvabbpwnwkrt.supabase.co";
-    const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlsem93Y2xuc3ZhYmJwd253a3J0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUzMTI2NjQsImV4cCI6MjA1MDg4ODY2NH0.KrPg77Bavax4DgHCpDou4L9XAB41II9_ywNcjy8w6ao'
-    supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
-    console.log(supabase);
-}
-
-async function insertRow(data) {
+function insertRow(data) {
     return supabase
       .from('PALETAS')
       .insert([
